0026fb4e2d2623cb8efa924b4211a7454df310b9
Malcolm Nunley <Mxnunley1@gmail.com>
2019-11-02 16:52:43 -0400

    initial commit, created all the components and added the routing.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import * as serviceWorker from "./serviceWorker";
+import { Route, BrowserRouter as Router } from "react-router-dom";
+import LoginComponent from "./components/login/login";
+import SignupComponent from "./components/signup/signup";
+import DashboardComponent from "./components/dashboard/dashboard";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const firebase = require("firebase");
+require("firebase/firestore");
+
+firebase.initializeApp({
+  apiKey: "AIzaSyAdctZaykHD3Gi0wJbO_h40bmT6OzDDJPo",
+  authDomain: "react-chatapp-abed5.firebaseapp.com",
+  databaseURL: "https://react-chatapp-abed5.firebaseio.com",
+  projectId: "react-chatapp-abed5",
+  storageBucket: "react-chatapp-abed5.appspot.com",
+  messagingSenderId: "515944464381",
+  appId: "1:515944464381:web:1e1d7ee10d077d26d6a394"
+});
+
+const routing = (
+  <Router>
+    <div id="routing-container">
+      <Route path="/login" component={LoginComponent}></Route>
+      <Route path="/signup" component={SignupComponent}></Route>
+      <Route path="/dashboard" component={DashboardComponent}></Route>
+    </div>
+  </Router>
+);
+
+ReactDOM.render(routing, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
