71388173c6bce76d561e353a9b60682b788acf22
Malcolm Nunley <Mxnunley1@gmail.com>
2019-11-03 12:59:17 -0500

    added 2 more component and integrated the chats into the view

--- src/components/dashboard/dashboard.js
+++ src/components/dashboard/dashboard.js
@@ -1,9 +1,78 @@
 import React from "react";
+import ChatListComponent from "../chatList/chatList";
+import ChatViewComponent from "../chatview/chatView";
+import { Button, withStyles } from "@material-ui/core";
+import styles from "./styles";
+
+const firebase = require("firebase");
 
 class DashboardComponent extends React.Component {
-  render() {
-    return <div>Hello world from the Dashboard</div>;
+  constructor() {
+    super();
+    this.state = {
+      selectedChat: null,
+      newChatFormVisible: false,
+      email: null,
+      chats: []
+    };
   }
+
+  render() {
+    const { classes } = this.props;
+
+    return (
+      <div>
+        <ChatListComponent
+          history={this.props.history}
+          newChatBtnClicked={this.newChatBtnClicked}
+          selectChatFn={this.selectChat}
+          chats={this.state.chats}
+          userEmail={this.state.email}
+          selectedChatIndex={this.state.selectedChat}
+        ></ChatListComponent>
+        {this.state.newChatFormVisible ? null : (
+          <ChatViewComponent
+            user={this.state.email}
+            chat={this.state.chats[this.state.selectedChat]}
+          ></ChatViewComponent>
+        )}
+        <Button className={classes.signOutBtn} onClick={this.signOut}>
+          Sign Out
+        </Button>
+      </div>
+    );
+  }
+
+  signOut = () => firebase.auth().signOut();
+
+  selectChat = chatIndex => {
+    this.setState({ selectedChat: chatIndex });
+  };
+
+  newChatBtnClicked = () =>
+    this.setState({
+      newChatFormVisible: true,
+      selectedChat: null
+    });
+  componentDidMount = () => {
+    firebase.auth().onAuthStateChanged(async _usr => {
+      if (!_usr) this.props.history.push("/login");
+      else {
+        await firebase
+          .firestore()
+          .collection("chats")
+          .where("users", "array-contains", _usr.email)
+          .onSnapshot(async res => {
+            const chats = res.docs.map(_doc => _doc.data());
+            await this.setState({
+              email: _usr.email,
+              chats: chats
+            });
+            console.log(this.state);
+          });
+      }
+    });
+  };
 }
 
-export default DashboardComponent;
+export default withStyles(styles)(DashboardComponent);
